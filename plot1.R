8764f7c646c5b895e7e96c9801a3213c9a4a0430
RajeshRaoBN <mailrbn@gmail.com>
2014-07-13 22:38:21 +1000

    Plot1
    Commit Plot1

---
+++ plot1.R
@@ -0,0 +1,29 @@
+# 1) Download the dataset from https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+# 2) Extract the text file and save it in ~/R/Coursera/ExploratoryAnalysis/Project1 directory
+
+# Set working directory
+setwd("~/R/Coursera/ExploratoryAnalysis/Project1")
+
+# Install sqldf package and open the library
+library(sqldf)
+
+# Load the dataset into R as required and perform the transformation
+# When loading the dataset into R, please consider the following:
+#   
+# The dataset has 2,075,259 rows and 9 columns. First calculate a rough estimate of how much memory the dataset will require in memory before reading into R. Make sure your computer has enough memory (most modern computers should be fine).
+# 
+# We will only be using data from the dates 2007-02-01 and 2007-02-02. One alternative is to read the data from just those dates rather than reading in the entire dataset and subsetting to those dates.
+# 
+# You may find it useful to convert the Date and Time variables to Date/Time classes in R using the strptime() and as.Date() functions.
+# 
+# Note that in this dataset missing values are coded as ?.
+hps = read.csv.sql("household_power_consumption.txt", sep = ";", sql = "SELECT * FROM file WHERE Date = '1/2/2007' or Date = '2/2/2007'")
+transform(hps, Date = as.Date(strptime(paste(hps[,"Date"] , hps[,"Time"] ), format = "%d/%m/%Y %T")))
+transform(hps, Global_active_power = as.numeric(Global_active_power))
+
+# Generate Plot1 as per instruction
+# Our overall goal here is simply to examine how household energy usage varies over a 2-day period in February, 2007. Your task is to reconstruct the following plots below, all of which were constructed using the base plotting system.
+# Construct the plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels.
+hist(hps[,"Global_active_power"], col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.copy(png,'./plot1.png')
+dev.off()
